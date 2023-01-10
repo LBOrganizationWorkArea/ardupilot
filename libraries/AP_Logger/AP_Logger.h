d36c1c119e0131e9228ad73cf86d728595afb0cc
Davide_Lentini <davidelentini@gmail.com>
2023-01-10 10:42:36 +0100

    LuminousBees : Changes for Indoor and Outdoor - OTA modifications ( Modified bl_protocol.cpp file) - Added Hwdefs luminousbee5 , luminousbee-mini2 (modifications with 42605 and 42688 ) , including bootloaders with modified OTA - Added indoor files (with fix for 4.3) - Added modification in AP_Logger , for high frequency log acquisition of indoor positioning

--- libraries/AP_Logger/AP_Logger.h
+++ libraries/AP_Logger/AP_Logger.h
@@ -332,6 +332,8 @@ public:
     void Write_PSCE(float pos_target, float pos, float vel_desired, float vel_target, float vel, float accel_desired, float accel_target, float accel);
     void Write_PSCD(float pos_target, float pos, float vel_desired, float vel_target, float vel, float accel_desired, float accel_target, float accel);
 
+    /* Write Raw_Pos at current time */
+    void WriteRawPos(float x, float y , float z);
     void Write(const char *name, const char *labels, const char *fmt, ...);
     void Write(const char *name, const char *labels, const char *units, const char *mults, const char *fmt, ...);
     void WriteStreaming(const char *name, const char *labels, const char *fmt, ...);
